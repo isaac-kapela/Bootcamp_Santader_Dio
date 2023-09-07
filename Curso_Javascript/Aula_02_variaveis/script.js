10c8e6fc37b761c8dcafe42261ecb8c68ea99e38
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-09-07 19:47:22 -0300

    curso de js bootcamp

---
+++ Curso_Javascript/Aula_02_variaveis/script.js
@@ -0,0 +1,27 @@
+// Tipos primiitivos
+
+//tipo boolean
+var oi = false;
+console.log(typeof(oi));
+
+//tipo number
+var numero =1;
+console.log(typeof(numero));
+
+//tipo string
+var nome = "isaac";
+console.log(typeof(nome));
+
+//var
+var variavel = 'isaac';
+variavel = "kapela";
+console.log(variavel);
+
+// let
+let variavel_1 = 'dizolele';
+variavel_1 = 'João'
+console.log(variavel_1);
+
+//const
+const variavel_2 = "Manzambi";
+console.log(variavel_2);
---
+++ Curso_Javascript/Aula_03/script.js
@@ -0,0 +1,13 @@
+// Arrys e vetores tem que ser declarados por colchetos [tft]
+
+let arry = ["iae",1,false];
+console.log(arry);
+
+//arry dentro de arry
+let oi = ["iae",11,true,["arry1"],['arry2']];
+console.log(oi);
+
+
+arry.forEach()
+
+//Objetos tem ser declarados entre chaves {}
